17fe5c6109d74ecb40140b3ff9402c2bb397ae89
brunofelixdesousa <brunofelixdesousa@hotmail.com>
2022-03-05 20:21:04 -0300

    refatorando classe Curso e Mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,30 @@
-import br.com.dio.desafio.dominio.Bootcamp;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Dev;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
+
+        Curso cursoJava = new Curso();
+        cursoJava.setTitulo("Curso Java");
+        cursoJava.setDescricao("Seja fullstack developer");
+        cursoJava.setCargaHoraria(400);
+
+        Curso cursoSpring = new Curso();
+        cursoSpring.setTitulo("Curso Spring");
+        cursoSpring.setDescricao("Desenvolva com Spring");
+        cursoSpring.setCargaHoraria(300);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria java");
+        mentoria.setDescricao("Desenvolva-se com a melhor linguagem do mercado.");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(cursoJava);
+        System.out.println(cursoSpring);
+        System.out.println(mentoria);
+
+        /*
         Curso curso1 = new Curso();
         curso1.setTitulo("curso java");
         curso1.setDescricao("descrição curso java");
@@ -22,9 +40,6 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        /*System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);*/
 
         Bootcamp bootcamp = new Bootcamp();
         bootcamp.setNome("Bootcamp Java Developer");
@@ -57,6 +72,8 @@ public class Main {
         System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
         System.out.println("Conteúdos Concluidos João:" + devJoao.getConteudosConcluidos());
         System.out.println("XP:" + devJoao.calcularTotalXp());
+
+        */
 
     }
 
